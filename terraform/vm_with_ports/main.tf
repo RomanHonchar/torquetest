4fe797e1aa535e3e443d0f3e97a9a71f9b40a0dc
RomanHonchar <141116263+RomanHonchar@users.noreply.github.com>
2024-01-15 19:00:17 +0200

    Create main.tf

---
+++ terraform/vm_with_ports/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable "ami_centos" {
+  type = string
+  default = "ami-0b14f0a2730f8cbd2"
+}
+
+resource "aws_instance" "ec2" {
+  ami = var.ami_centos
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.webtraffic.name]
+  key_name = "EC2 Test"
+}
+
+resource "aws_security_group" "webtraffic" {
+  name = "Allow HTTPS"
+
+  vpc_id = "vpc-00aefe61eff2d0c23"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["94.45.70.5/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+output "ec2_ip" {
+  value = aws_instance.ec2.public_ip
+}
